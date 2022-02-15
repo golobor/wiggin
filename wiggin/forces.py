df5c46895a9e419cda7793272625e289b1095137
Anton Goloborodko <goloborodko.anton@gmail.com>
2022-02-15 14:36:14 +0100

    forces: add adjustable harmonic bonds

--- wiggin/forces.py
+++ wiggin/forces.py
@@ -1,4 +1,87 @@
-from polychrom.forces import openmm
+import collections
+from polychrom.forces import openmm, _check_bonds
+
+
+def adjustable_harmonic_bonds(
+    sim_object,
+    bonds,
+    bondWiggleDistance=0.05,
+    bondLength=1.0,
+    name="adjustable_harmonic_bonds",
+    override_checks=False,
+):
+    """
+    
+    Constant force bond force. Energy is roughly linear with estension 
+    after r=quadraticPart; before it is quadratic to make sure the force
+    is differentiable. 
+    
+    Force is parametrized using the same approach as bond force:
+    it reaches U=kT at extension = bondWiggleDistance 
+    
+    Note that, just as with bondForce, mean squared extension 
+    is actually larger than wiggleDistance by sqrt(2) factor. 
+    
+    Parameters
+    ----------
+    
+    bonds : iterable of (int, int)
+        Pairs of particle indices to be connected with a bond.
+    bondWiggleDistance : float
+        Displacement at which bond energy equals 1 kT. 
+        Can be provided per-particle.
+    bondLength : float
+        The length of the bond.
+        Can be provided per-particle.
+    override_checks: bool
+        If True then do not check that no bonds are repeated.
+        False by default.
+    """
+
+    # check for repeated bonds
+    if not override_checks:
+        _check_bonds(bonds, sim_object.N)
+
+    energy = (
+        "kT * ((r / l_unit - r0) / wiggle_dist)^2"
+    )
+    force = openmm.CustomBondForce(energy)
+    force.name = name
+
+    force.addGlobalParameter("l_unit", sim_object.conlen)
+    force.addGlobalParameter("kT", sim_object.kT)
+
+    bond_params = []
+
+    if isinstance(bondWiggleDistance, collections.abc.Iterable):
+        force.addPerBondParameter("wiggle_dist")
+        bond_params.append(bondWiggleDistance)
+    else:
+        force.addGlobalParameter("wiggle_dist", bondWiggleDistance)
+
+    if isinstance(bondLength, collections.abc.Iterable):
+        force.addPerBondParameter("r0")
+        bond_params.append(bondLength)
+    else:
+        force.addGlobalParameter("r0", bondLength)
+
+    if bond_params:
+        bond_params = list(zip(*bond_params))
+
+    for bondIdx, (i, j) in enumerate(bonds):
+        if (i >= sim_object.N) or (j >= sim_object.N):
+            raise ValueError(
+                "\nCannot add bond with monomers %d,%d that"
+                "are beyound the polymer length %d" % (i, j, sim_object.N)
+            )
+
+        force.addBond(
+            int(i),
+            int(j),
+            bond_params[bondIdx] if bond_params else []
+        )
+
+    return force
 
 
 def quartic_repulsive(sim_object, trunc=3.0, radiusMult=1.0, name="quartic_repulsive"):
